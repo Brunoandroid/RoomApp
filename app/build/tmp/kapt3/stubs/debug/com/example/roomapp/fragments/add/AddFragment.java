99f677167be15e4a1e78aefb53a72b502a7852e9
Bruno Freitas Araujo <null96@outlook.com>
2020-12-12 01:36:21 -0300

    Concluido

---
+++ app/build/tmp/kapt3/stubs/debug/com/example/roomapp/fragments/add/AddFragment.java
@@ -0,0 +1,48 @@
+package com.example.roomapp.fragments.add;
+
+import java.lang.System;
+
+@kotlin.Metadata(mv = {1, 1, 16}, bv = {1, 0, 3}, k = 1, d1 = {"\u0000H\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\b\u0005\n\u0002\u0010\u000b\n\u0000\n\u0002\u0010\u000e\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0004\u0018\u00002\u00020\u0001B\u0005\u00a2\u0006\u0002\u0010\u0002J \u0010\t\u001a\u00020\n2\u0006\u0010\u000b\u001a\u00020\f2\u0006\u0010\r\u001a\u00020\f2\u0006\u0010\u000e\u001a\u00020\u000fH\u0002J\b\u0010\u0010\u001a\u00020\u0011H\u0002J&\u0010\u0012\u001a\u0004\u0018\u00010\u00132\u0006\u0010\u0014\u001a\u00020\u00152\b\u0010\u0016\u001a\u0004\u0018\u00010\u00172\b\u0010\u0018\u001a\u0004\u0018\u00010\u0019H\u0016J\u001a\u0010\u001a\u001a\u00020\u00112\u0006\u0010\u001b\u001a\u00020\u00132\b\u0010\u0018\u001a\u0004\u0018\u00010\u0019H\u0016J\b\u0010\u001c\u001a\u00020\u0011H\u0002R\u001a\u0010\u0003\u001a\u00020\u0004X\u0086.\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u0005\u0010\u0006\"\u0004\b\u0007\u0010\b\u00a8\u0006\u001d"}, d2 = {"Lcom/example/roomapp/fragments/add/AddFragment;", "Landroidx/fragment/app/Fragment;", "()V", "mUserviewModel", "Lcom/example/roomapp/viewmodel/UserViewModel;", "getMUserviewModel", "()Lcom/example/roomapp/viewmodel/UserViewModel;", "setMUserviewModel", "(Lcom/example/roomapp/viewmodel/UserViewModel;)V", "inputCheck", "", "firstName", "", "lastName", "age", "Landroid/text/Editable;", "insertDataToDatabase", "", "onCreateView", "Landroid/view/View;", "inflater", "Landroid/view/LayoutInflater;", "container", "Landroid/view/ViewGroup;", "savedInstanceState", "Landroid/os/Bundle;", "onViewCreated", "view", "teste", "app_debug"})
+public final class AddFragment extends androidx.fragment.app.Fragment {
+    @org.jetbrains.annotations.NotNull()
+    public com.example.roomapp.viewmodel.UserViewModel mUserviewModel;
+    private java.util.HashMap _$_findViewCache;
+    
+    @org.jetbrains.annotations.NotNull()
+    public final com.example.roomapp.viewmodel.UserViewModel getMUserviewModel() {
+        return null;
+    }
+    
+    public final void setMUserviewModel(@org.jetbrains.annotations.NotNull()
+    com.example.roomapp.viewmodel.UserViewModel p0) {
+    }
+    
+    @org.jetbrains.annotations.Nullable()
+    @java.lang.Override()
+    public android.view.View onCreateView(@org.jetbrains.annotations.NotNull()
+    android.view.LayoutInflater inflater, @org.jetbrains.annotations.Nullable()
+    android.view.ViewGroup container, @org.jetbrains.annotations.Nullable()
+    android.os.Bundle savedInstanceState) {
+        return null;
+    }
+    
+    @java.lang.Override()
+    public void onViewCreated(@org.jetbrains.annotations.NotNull()
+    android.view.View view, @org.jetbrains.annotations.Nullable()
+    android.os.Bundle savedInstanceState) {
+    }
+    
+    private final void teste() {
+    }
+    
+    private final void insertDataToDatabase() {
+    }
+    
+    private final boolean inputCheck(java.lang.String firstName, java.lang.String lastName, android.text.Editable age) {
+        return false;
+    }
+    
+    public AddFragment() {
+        super();
+    }
+}
